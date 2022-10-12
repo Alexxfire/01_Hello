4b83e94ee15bcf21b3ae6047c3b80d6c0d8bf2dc
aogonkov <fire5@mail.ru>
2022-10-12 22:26:57 +0300

    Пример деления чисел

---
+++ 03_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
+double result = numberA % numberB; //остаток от деления
+Console.WriteLine (result);
