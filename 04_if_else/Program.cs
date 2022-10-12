28ddb9418e4bd7cc893bce9b143154daa1f675d5
aogonkov <fire5@mail.ru>
2022-10-12 22:41:00 +0300

    Пример IF ELSE с именем

---
+++ 04_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "alex")
+{
+    Console.WriteLine ("УРАААА!!!");
+}
+else 
+{
+    Console.WriteLine ("Пока");
+}
